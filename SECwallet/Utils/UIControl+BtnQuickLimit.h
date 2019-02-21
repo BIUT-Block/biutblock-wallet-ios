2c1f52f56b77a6886f6aa69bab26b4089a64d7b0
Laughing <xiangwu1030@126.com>
2019-02-21 16:23:52 +0800

    优化UIControl+BtnQuickLimit文件

--- SECwallet/Utils/UIControl+BtnQuickLimit.h
+++ SECwallet/Utils/UIControl+BtnQuickLimit.h
@@ -9,9 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface UIControl (BtnQuickLimit)
-// 间隔多少秒才能响应事件
-@property(nonatomic, assign) NSTimeInterval  acceptEventInterval;
-//是否能执行方法
-@property(nonatomic, assign) BOOL ignoreEvent;
+
+@property (nonatomic,assign) NSTimeInterval timeInterval;
+@property (nonatomic,assign) BOOL isIgnoreEvent;
 
 @end
--- SECwallet/Utils/UIControl+BtnQuickLimit.m
+++ SECwallet/Utils/UIControl+BtnQuickLimit.m
@@ -8,51 +8,60 @@
 
 #import "UIControl+BtnQuickLimit.h"
 #import <objc/runtime.h>
-static const char * UIControl_acceptEventInterval = "UIControl_acceptEventInterval";
-static const char * UIControl_ignoreEvent = "UIControl_ignoreEvent";
+#define defaultInterval 0.25    //默认时间间隔
 
 @implementation UIControl (BtnQuickLimit)
 
--(void)setAcceptEventInterval:(double)acceptEventInterval {
-    //关联属性对象
-    objc_setAssociatedObject(self, UIControl_acceptEventInterval,@(acceptEventInterval), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+- (NSTimeInterval)timeInterval{
+    return [objc_getAssociatedObject(self,_cmd)doubleValue];
 }
 
--(NSTimeInterval)acceptEventInterval{
-    return [objc_getAssociatedObject(self, UIControl_acceptEventInterval) doubleValue];
+- (void)setTimeInterval:(NSTimeInterval)timeInterval{
+    objc_setAssociatedObject(self,@selector(timeInterval),@(timeInterval),OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (void)setIsIgnoreEvent:(BOOL)isIgnoreEvent{
+    objc_setAssociatedObject(self,@selector(isIgnoreEvent),@(isIgnoreEvent),OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
--(void)setIgnoreEvent:(BOOL)ignoreEvent {
-    //关联属性对象
-    objc_setAssociatedObject(self, UIControl_ignoreEvent, @(ignoreEvent), OBJC_ASSOCIATION_ASSIGN);
+- (BOOL)isIgnoreEvent{
+    return [objc_getAssociatedObject(self,_cmd)boolValue];
 }
 
--(BOOL)ignoreEvent{
-    return [objc_getAssociatedObject(self, UIControl_ignoreEvent) boolValue];
+- (void)resetState{
+    [self setIsIgnoreEvent:NO];
 }
 
-//所有类即将加入内存的时候都会走load方法，所以我们在这个里面交换方法
-+(void)load{
-    Method a = class_getInstanceMethod(self, @selector(sendAction:to:forEvent:));
-    Method b = class_getInstanceMethod(self, @selector(swizzing_sendAction:to:forEvent:));
-    method_exchangeImplementations(a, b);
++ (void)load{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        SEL selA = @selector(sendAction:to:forEvent:);
+        SEL selB = @selector(mySendAction:to:forEvent:);
+        Method methodA = class_getInstanceMethod(self, selA);
+        Method methodB = class_getInstanceMethod(self, selB);
+        //将methodB的实现添加到系统方法中也就是说将methodA方法指针添加成方法methodB的返回值表示是否添加成功
+        BOOL isAdd = class_addMethod(self, selA,method_getImplementation(methodB),method_getTypeEncoding(methodB));
+        //添加成功了说明本类中不存在methodB所以此时必须将方法b的实现指针换成方法A的，否则b方法将没有实现。
+        if(isAdd) {
+            class_replaceMethod(self, selB,method_getImplementation(methodA),method_getTypeEncoding(methodA));
+        }else{
+            //添加失败了说明本类中有methodB的实现，此时只需要将methodA和methodB的IMP互换一下即可。
+            method_exchangeImplementations(methodA, methodB);
+        }
+    });
 }
 
--(void)swizzing_sendAction:(SEL)action to:(id)tagert forEvent:(UIEvent*)event{
-    if (self.ignoreEvent) {
-        NSLog(@"你点击的太快了");
-        return;
+- (void)mySendAction:(SEL)action to:(id)target forEvent:(UIEvent*)event{
+    if([NSStringFromClass(self.class) isEqualToString:@"UIButton"]) {
+        self.timeInterval = self.timeInterval == 0 ? defaultInterval:self.timeInterval;
+        if(self.isIgnoreEvent){
+            return;
+        }else if(self.timeInterval > 0){
+            [self performSelector:@selector(resetState)withObject:nil afterDelay:self.timeInterval];
+        }
     }
-    //acceptEventInterval 的数据必须大于0，否则会一直走调用的方法
-    if (self.acceptEventInterval>0) {
-        self.ignoreEvent = YES;
-        [self performSelector:@selector(setIgnoreWithNo) withObject:nil afterDelay:self.acceptEventInterval];
-    }
-    //当前按钮调用该方法
-    [self swizzing_sendAction:action to:tagert forEvent:event];
-}
--(void)setIgnoreWithNo{
-    self.ignoreEvent = NO;
+    //此处methodA和methodB方法IMP互换了，实际上执行sendAction；所以不会死循环
+    self.isIgnoreEvent = YES;
+    [self mySendAction:action to:target forEvent:event];
 }
 
 @end
